f9a68abca3d0f48019c717e8521307ded10307ed
marius.morar <marius.morar02@gmail.com>
2021-02-18 22:29:33 +0200

    Add a new SplashActivity activity

---
+++ QuizApp/Activities/SplashActivity.cs
@@ -0,0 +1,15 @@
+﻿using Android.App;
+
+namespace QuizApp.Activities
+{
+    [Activity(Theme = "@style/MyTheme.Splash", MainLauncher = true, NoHistory = true)]
+    public class SplashActivity : Activity
+    {
+        protected override void OnResume()
+        {
+            base.OnResume();
+
+            StartActivity(typeof(MainActivity));
+        }
+    }
+}
