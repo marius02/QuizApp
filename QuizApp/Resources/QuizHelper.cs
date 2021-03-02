df76fd4869626661dd6ac2ebbe478fb0d8a5935b
marius.morar <marius.morar02@gmail.com>
2021-03-02 21:23:59 +0200

    Add QuizHelper.cs class

---
+++ QuizApp/Resources/QuizHelper.cs
@@ -0,0 +1,21 @@
+﻿namespace QuizApp.Resources
+{
+    public class QuizHelper
+    {
+        public string GetTopicDescription(string topic)
+        {
+            var topicDescription = topic switch
+            {
+                "History" => "History description",
+                "Geography" => "Geography description",
+                "Space" => "Space description",
+                "Business" => "Business description",
+                "Engineering" => "Engineering description",
+                "Programming" => "Programming description",
+                _ => "General Description"
+            };
+
+            return topicDescription;
+        }
+    }
+}
